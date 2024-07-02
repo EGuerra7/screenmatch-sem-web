81de06ca400da7d93b9358d1a1413297029fca70
Erick Guerra <erickmuzzi@gmail.com>
2024-07-02 20:28:47 -0300

    first commit

---
+++ screenmatch/src/test/java/br/com/guerra/screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guerra.screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
