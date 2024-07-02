7aae7c2b1959d8553d836156b72a791489cc69c4
EGuerra7 <erickmuzzi@gmail.com>
2024-07-02 20:54:42 -0300

    episodio

--- screenmatch/src/main/java/br/com/guerra/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/guerra/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.guerra.screenmatch.model.DadosEpisodio;
 import br.com.guerra.screenmatch.model.DadosSerie;
 import br.com.guerra.screenmatch.service.ConsumoApi;
 import br.com.guerra.screenmatch.service.ConverteDados;
@@ -23,6 +24,9 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
+		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=74170ce1");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
 
 	}
 
